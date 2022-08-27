40ef4cb8f4417e4a214dbff3f99c61eafb92d60e
dpfaker01 <112249907+dpfaker01@users.noreply.github.com>
2022-08-27 23:45:41 +0800

    Add files via upload

---
+++ videomaker.py
@@ -0,0 +1,39 @@
+'''
+Author: Naiyuan liu
+Github: https://github.com/NNNNAI
+Date: 2021-11-23 17:03:58
+LastEditors: Naiyuan liu
+LastEditTime: 2021-11-24 19:19:52
+Description: 
+'''
+import os 
+import cv2
+import glob
+import torch
+import shutil
+import numpy as np
+from tqdm import tqdm
+from util.reverse2original import reverse2wholeimage
+import moviepy.editor as mp
+from moviepy.editor import AudioFileClip, VideoFileClip 
+from moviepy.video.io.ImageSequenceClip import ImageSequenceClip
+import  time
+from util.add_watermark import watermark_image
+from util.norm import SpecificNorm
+from parsing_model.model import BiSeNet
+
+
+    video.release()
+
+    # image_filename_list = []
+    path = os.path.join(temp_results_dir,'*.jpg')
+    image_filenames = sorted(glob.glob(path))
+
+    clips = ImageSequenceClip(image_filenames,fps = fps)
+
+    if not no_audio:
+        clips = clips.set_audio(video_audio_clip)
+
+
+    clips.write_videofile(save_path,audio_codec='aac')
+
